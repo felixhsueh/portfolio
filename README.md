6ae5e377c363f5bc7f768a49d6f53eef40cfd15c
Zekui <zekui.xue@bath.edu>
2025-05-08 06:48:36 +0800

    portfolio
    portfolio

---
+++ README.md
@@ -0,0 +1,16 @@
+# portfolio - Overview 🚀
+
+This repository contains the open source version of my porfolio.
+Do check it out!
+
+## Instructions 🛠️
+
+Gsap club plugins check out here: https://gsap.com/docs/v3/Installation/
+
+**Techstack** - React, TypeScript, GSAP, ThreeJS, WebGL, HTML, Css, JavaScript
+
+
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+          labore sit non ipsum temporibus quidem, deserunt eaque officiis
+          mollitia ratione suscipit repellat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
